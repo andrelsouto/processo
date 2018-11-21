47f4f07df5b8f14f88605e553863cff9119b3359
André Luiz <a.luiz.souto@gmail.com>
2018-11-21 01:08:32 -0300

    .

--- src/main/java/br/com/andre/processos/configuration/Configuration.java
+++ src/main/java/br/com/andre/processos/configuration/Configuration.java
@@ -1,5 +1,7 @@
 package br.com.andre.processos.configuration;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,7 +20,8 @@ public class Configuration {
 			public void addCorsMappings(CorsRegistry registry) {
 				
 				registry.addMapping("/**")
-				.allowedOrigins("https://processo.herokuapp.com", "https://glacial-brushlands-71010.herokuapp.com")
+				.allowedOrigins("https://processo.herokuapp.com")
+				.allowedOrigins("https://glacial-brushlands-71010.herokuapp.com")
 				.allowedHeaders("Access-Control-Allow-Origin", "Content-Disposition", "User-Agent")
 				.allowedMethods("POST", "DELETE", "GET", "PATCH", "PUT", "UPDATE")
 				.allowCredentials(false).maxAge(3600);
