4ace110cab430c701198bb8d2b4b096c6b4a9027
Andre <a.luiz.souto@gmail.com>
2019-05-19 05:29:36 -0300

    relatorio e sentenciar

--- src/main/java/br/com/andre/processos/utils/PDFReport.java
+++ src/main/java/br/com/andre/processos/utils/PDFReport.java
@@ -1,24 +1,30 @@
 package br.com.andre.processos.utils;
 
-import java.util.Map;
-
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ResourceUtils;
+
+import java.io.FileNotFoundException;
+import java.util.Map;
 
 public class PDFReport {
 
-	
+	@Autowired
+	ApplicationContext context;
 	public byte[] reportGenarator(String fileName, Map<String, Object> params, JRBeanCollectionDataSource dataSource) {
 		
 		try {
 			
 			JasperPrint print = JasperFillManager.fillReport(
-					this.getClass().getClassLoader().getResource("/relatorios") + "/processos.jasper", params, dataSource);
+					ResourceUtils.getFile("classpath:relatorios/processos.jasper").getAbsolutePath(), params, dataSource);
 			return JasperExportManager.exportReportToPdf(print);
-		} catch (JRException e) {
+		} catch (JRException | FileNotFoundException e) {
 			
 			e.printStackTrace();
 			return null;
